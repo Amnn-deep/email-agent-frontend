30a8bdcb35e3403bf1b6348dccd79051cd6062ce
Yash Duhan <yash2004duhan@gmail.com>
2025-07-29 21:47:46 +0530

    changed api

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
-// const API_BASE_URL = 'https://email-agent-backendd.vercel.app';
+const API_BASE_URL = 'https://email-agent-backendd.vercel.app';
 
-const API_BASE_URL = 'http://localhost'
+// const API_BASE_URL = 'http://localhost'
 
 export { API_BASE_URL }; 
